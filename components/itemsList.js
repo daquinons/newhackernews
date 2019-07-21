ad3b87241f51695b741cbabfdbd2ac2128cd6065
David Quiñones <daquinons@users.noreply.github.com>
2019-07-21 22:27:07 +0200

    Add InfiniteScroll component

--- components/itemsList.js
+++ components/itemsList.js
@@ -1,12 +1,29 @@
+import InfiniteScroll from 'react-infinite-scroll-component';
 import Item from './item';
 
 const ItemsList = ({ items }) => {
   return (
-    <div>
+    <InfiniteScroll
+      dataLength={items.length}
+      next={undefined}
+      hasMore={true}
+      loader={
+        <div className="d-flex justify-content-center">
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      }
+    >
       {items.map((item, index) => {
         return <Item key={index} item={item} />;
       })}
-    </div>
+      <style jsx>{`
+        .d-flex {
+          margin-bottom: 2rem;
+        }
+      `}</style>
+    </InfiniteScroll>
   );
 };
 
